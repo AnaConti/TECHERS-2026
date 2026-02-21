3760b7993f1876e20f2c7e414d1ed3f7c773dc07
AnaConti <anabarbara.conti@gmail.com>
2026-02-21 09:17:27 -0300

    feat: aula 3 da turma de sexta add

---
+++ Aulas/Web-Back_Sexta/Aula 3/app.js
@@ -0,0 +1,42 @@
+// ## Exercícios com Funções
+
+// ### 1. O Conversor de Moedas
+
+// Crie uma função chamada `converterDolar` que recebe um valor em Reais. Dentro dela, calcule o valor em Dólar (considere  para facilitar). A função deve exibir: "O valor em dólar é: $US$ [resultado]".
+
+// ### 2. Calculadora de Área de Terreno
+
+// Crie uma função chamada `calcularArea` que recebe dois parâmetros: `largura` e `comprimento`. A função deve calcular a área total e exibir: "A área do terreno é de [resultado] metros quadrados".
+
+// ### 3. Sistema de Média Escolar
+
+// Crie uma função chamada `verificarAprovacao` que recebe três notas.
+
+// * Calcule a média.
+// * Se a média for 7 ou mais, exiba "Aprovado!".
+// * Caso contrário, exiba "Reprovado, estude mais!".
+
+// ### 4. Gerador de E-mail Acadêmico
+
+// Crie uma função que recebe o `nome` e o `sobrenome` de um aluno. Ela deve retornar (ou exibir) o e-mail no formato: `nome.sobrenome@escola.com`.
+
+// ### 5. O Verificador de Par ou Ímpar
+
+// Crie uma função chamada `ehPar` que recebe um número.
+
+// * Se o número for par, exiba "O número [x] é par".
+// * Se for ímpar, exiba "O número [x] é ímpar".
+// *(Dica: use o operador de resto `% 2 == 0`)*.
+
+// ### 6. Aplicador de Descontos
+
+// Crie uma função chamada `aplicarDesconto` que recebe o `valorProduto` e a `porcentagemDesconto`. Ela deve calcular o novo preço e exibir: "Com [x]% de desconto, o produto custa R$ [novoValor]".
+
+// ### 7. Conversor de Temperatura (Desafio)
+
+// Crie uma função que recebe uma temperatura em Celsius e a converte para Fahrenheit.
+
+
+// ### 8. Validador de Senha Forte
+
+// Crie uma função chamada `validarSenha` que recebe uma string. Se a senha tiver 8 caracteres ou mais, exiba "Senha segura". Se tiver menos, exiba "Senha muito curta".
